ddbc2ea4d6aa4de240260dd8caa349b24ed2f237
Hakimuddin Kika <hakimuddin.kika92@gmail.com>
2024-10-24 19:05:48 +0300

    Adding Metadata in Layout,Invoice Page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from './ui/fonts';
 
+export const metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Hakimuddin Kika - Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://nextjs-dashboard-learn-tau.vercel.app/'),
+};
+
 export default function RootLayout({children}) {
   return (
     <html lang="en">
